4f899f5e975a99207774ca034b7abb888976e028
YogaGit112 <111202415635@mhs.dinus.ac.id>
2026-06-27 23:11:24 +0700

    menambahkan api data produk dan transaksi

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -27,7 +27,13 @@
             </li><!-- End Produk Nav -->
         <?php
         }
-        ?>     
+        ?>
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+                <i class="bi bi-person"></i>
+                <span>History</span>
+            </a>
+        </li><!-- End History Nav -->     
     </ul>
 
 </aside><!-- End Sidebar-->
